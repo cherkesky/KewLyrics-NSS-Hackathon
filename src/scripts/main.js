00950f14c09316279fb74b043b90115d4ca1c2fd
Dylan <Dylan.Griff11@gmail.com>
2019-10-26 15:10:58 -0500

    successful working API

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
-console.log("KewLyrics");
+const url = "https://api.musixmatch.com/ws/1.1"
+const textValue = "don't stop"
 
+APIManager = {
+    getArtist() {
+return fetchJsonp (`https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&q_lyrics=baby&quorum_factor=1&apikey=4cc92c98b858367876ca9869d3895f76`)
+.then(res => res.json())
+.then(res =>console.log(res))
+}
+}
+
+//.then(res => jsonIterator(jsonfiedResponse))
+
+APIManager.getArtist()
 
