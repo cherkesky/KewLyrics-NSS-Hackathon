d30f25e682dc448aaa60f39c08c7db95bcd78183
Cherkesky <guy@cherkesky.com>
2019-10-26 15:43:37 -0500

    jsonIterator function is completed. needs to be tested with real user input

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,54 +3,34 @@ const url = "https://api.musixmatch.com/ws/1.1"
 
 APIManager = {
     getArtist(textValue) {
-return fetchJsonp (`${url}/track.search?format=jsonp&q_lyrics=${textValue}&quorum_factor=1&apikey=4cc92c98b858367876ca9869d3895f76`)
+return fetchJsonp (`${url}/track.search?format=jsonp&q_lyrics=${textValue}&quorum_factor=1&apikey=${apiKey}`)
 .then(res => res.json())
-.then(res =>console.log(res))
+// .then(res =>console.log(res))
+.then (resjson=> jsonIterator(resjson, "tracks"))
 }
 }
-
-//.then(res => jsonIterator(jsonfiedResponse))
 
 APIManager.getArtist("don't stop")
-//initializing empty arrays
-trackArray=[]
-lyricsArray=[]
-
-dummyApiFetch=()=>{
-  console.log("dummyFetch API been called")
-fetch ("http://localhost:8088/message")
-.then(response=>response.json())
-.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse, "tracks"))
-}
-
-dummyApiFetch2=()=>{
-  console.log("dummyFetch API been called")
-fetch ("http://localhost:8088/message2")
-.then(response=>response.json())
-.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse, "lyrics"))
-}
 
 //***************************************************************************************************** 
 // a helper function that iterates through the json response and return the relavant items as an array
 //***************************************************************************************************** 
 
+//initializing empty arrays
+trackArray=[]
+lyricsArray=[]
+
 jsonIterator=(jsonfiedResponse, trackOrLyricsJSON)=>{
 console.log(jsonfiedResponse)
-
-if (trackOrLyricsJSON==="tracks"){   // we`re processing a response from the Track endpoint
-
-  for (let i=0; i<jsonfiedResponse.body.track_list.length; i++){
-    trackArray.push(jsonfiedResponse.body.track_list[i].track.track_id)
+if (trackOrLyricsJSON==="tracks"){   // we`re processing a response from the "tracks" endpoint
+  for (let i=0; i<jsonfiedResponse.message.body.track_list.length; i++){
+    trackArray.push(jsonfiedResponse.message.body.track_list[i].track.track_id)
   }  
   console.log(trackArray)
   return trackArray
-}else{ // we`re processing a response from the Lyrics endpoint
-
-// write code here
-
-console.log(lyricsArray)
+}else{ // we`re processing a response from the "lyrics" endpoint
+  lyricsArray.push(jsonfiedResponse.body.lyrics.lyrics_body)
+  console.log(lyricsArray)
   return lyricsArray
-}
-}
+}}
 
-dummyApiFetch();
