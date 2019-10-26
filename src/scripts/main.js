379d8c6a92eb40054b57d17c5edb137e36a764eb
Cherkesky <guy@cherkesky.com>
2019-10-26 14:45:54 -0500

    finished with the first part of the jsonIterator - the track array is working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,21 @@
 console.log("KewLyrics");
+trackArray=[]
+lyricsArray=[]
 
-
+dummyApiFetch=()=>{
+  console.log("dummyFetch API been called")
+fetch ("http://localhost:8088/message")
+.then(response=>response.json())
+.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse))
+}
 
 jsonIterator=(jsonfiedResponse)=>{
-for (let i=0; i<jsonfiedResponse.length; i++){
-console.log(jsonfiedResponse[i].message.body.track_list["track"].track_id);
-
-
-
+console.log(jsonfiedResponse)
+for (let i=0; i<jsonfiedResponse.body.track_list.length; i++){
+  trackArray.push(jsonfiedResponse.body.track_list[i].track.commontrack_id)
+}  
+console.log(trackArray)
+return trackArray
 }
-}
+
+dummyApiFetch();
