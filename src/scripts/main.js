5b8cd3dff63139e896dcc57f523d4fb7128b6e37
Cherkesky <guy@cherkesky.com>
2019-10-26 13:15:31 -0500

    starting workingon the jsonIterator function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 console.log("KewLyrics");
 
 
+
+jsonIterator=(jsonfiedResponse)=>{
+for (let i=0; i<jsonfiedResponse.length; i++){
+console.log(jsonfiedResponse[i].message.body.track_list["track"].track_id);
+
+
+
+}
+}
