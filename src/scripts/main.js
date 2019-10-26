1324aa422ff4027edde7ad603d1c208fe71d95cf
Cherkesky <guy@cherkesky.com>
2019-10-26 15:09:49 -0500

    dummy json files been edited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 console.log("KewLyrics");
+
+//initializing empty arrays
 trackArray=[]
 lyricsArray=[]
 
@@ -6,16 +8,37 @@ dummyApiFetch=()=>{
   console.log("dummyFetch API been called")
 fetch ("http://localhost:8088/message")
 .then(response=>response.json())
-.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse))
+.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse, "tracks"))
 }
 
-jsonIterator=(jsonfiedResponse)=>{
+dummyApiFetch2=()=>{
+  console.log("dummyFetch API been called")
+fetch ("http://localhost:8088/message2")
+.then(response=>response.json())
+.then(jsonfiedResponse=>jsonIterator(jsonfiedResponse, "lyrics"))
+}
+
+//***************************************************************************************************** 
+// a helper function that iterates through the json response and return the relavant items as an array
+//***************************************************************************************************** 
+
+jsonIterator=(jsonfiedResponse, trackOrLyricsJSON)=>{
 console.log(jsonfiedResponse)
-for (let i=0; i<jsonfiedResponse.body.track_list.length; i++){
-  trackArray.push(jsonfiedResponse.body.track_list[i].track.commontrack_id)
-}  
-console.log(trackArray)
-return trackArray
+
+if (trackOrLyricsJSON==="tracks"){   // we`re processing a response from the Track endpoint
+
+  for (let i=0; i<jsonfiedResponse.body.track_list.length; i++){
+    trackArray.push(jsonfiedResponse.body.track_list[i].track.track_id)
+  }  
+  console.log(trackArray)
+  return trackArray
+}else{ // we`re processing a response from the Lyrics endpoint
+
+// write code here
+
+console.log(lyricsArray)
+  return lyricsArray
+}
 }
 
 dummyApiFetch();
