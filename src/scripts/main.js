f07743f20193e72540633b6f7803dbbb3e771d3b
Cherkesky <guy@cherkesky.com>
2019-10-27 11:50:13 -0500

    printer is working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -194,9 +194,6 @@ let mywindow = window.open('', 'PRINT', 'height=400,width=600');
   mywindow.document.write(document.getElementById("lyricsContainer").innerHTML);
   mywindow.document.write('</body></html>');
 
-  // mywindow.document.close(); // necessary for IE >= 10
-  // mywindow.focus(); // necessary for IE >= 10*/
-
   mywindow.print();
   mywindow.close();
 
